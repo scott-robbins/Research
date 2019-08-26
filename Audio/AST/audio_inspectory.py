f3303fa79e08e012987e33fc6e0ecb1cc859ac3f
TylersDurden <srobbin1@ithaca.edu>
2019-08-26 14:57:39 -0400

    audio style transfer experiments

---
+++ Audio/AST/audio_inspectory.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import Tkinter as Tk
+import numpy as np
+import audiolib
+import librosa
+import os
+
--- Audio/AST/audiolib.py
+++ Audio/AST/audiolib.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.io import wavfile
 from threading import Thread
 import numpy as np
+import librosa
 import time
 import os
 
@@ -59,4 +60,60 @@ def load_track(file_name, verbose):
 
     return track
 
+
+def analyze_music_library():
+    tic = time.time()
+    os.system('clear')
+    Tracks = []
+    # Load Samples
+    if os.path.isdir('SAMPLES'):
+        print '\033[3mLoading Sample Tracks\033[0m'
+        for n in os.listdir('SAMPLES'):
+            m = n.split('.')[0].replace("'", '') + '.wav'
+            track = 'SAMPLES/' + n
+            print 'Converting %s to WAV File ...' % ('SAMPLES/' + n)
+            cmd = 'ffmpeg -loglevel quiet -i %s -ar 44100 -ac 2 -b:a 192k %s' % (track, m)
+            os.system(cmd)
+            Tracks.append(load_track(m, True))
+            os.system('rm %s' % m)
+    else:
+        print 'Creating SAMPLES Directory [For Training Data]'
+        os.mkdir('SAMPLES')
+    print '================================================================='
+    # TODO: DEBUGGING - Choose a song at a time to analye
+    #  (eventually all sequentially)
+    print 'Which song To analyze?:'
+    n = 0
+    titles = {}
+    for title in Tracks:
+        n += 1
+        name = title['file']
+        data = title['audio']
+        samp = title['sample_rate']
+        titles[n] = [name, samp, np.array(data)]
+        print '\t\033[1m[%d]\033[0m %s' % (n, name)
+    choice = int(raw_input('Enter a selection: '))
+    try:
+        print 'Loading %s' % titles[choice][0]
+    except KeyError:
+        print '\033[1m\033[33mSomething broke...\033[0m'
+    L = data[:, 0]
+    R = data[:, 1]
+    return L, R, titles
+
+
+def read_audio_spectum(filename):
+    """
+    Reads wav file and produces spectrum
+    Fourier phases are ignored
+    :param filename:
+    :return stfft, file:
+    """
+    N_FFT = 2048
+    x, fs = librosa.load(filename)
+    S = librosa.stft(x, N_FFT)
+    p = np.angle(S)
+
+    S = np.log1p(np.abs(S[:, :430]))
+    return S, fs
 
